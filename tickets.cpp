895a10ba15963e4f6e39b9af8b1cd4d89de796c7
A9K2 <112696859+A9K2@users.noreply.github.com>
2024-03-26 21:08:37 +0300

    Update tickets.cpp

--- tickets.cpp
+++ tickets.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+include <iostream>
 #include <vector>
 #include <string>
 #include <Windows.h>
@@ -14,27 +14,78 @@ public:
     {
         if (movies.empty())
         {
-            cout << "����� �������" << endl;
+            cout << "Афіша порожня" << endl;
         }
         else
         {
+            int i = 1;
             for (const string& movie : movies)
             {
-                cout << "-" << movie << endl;
+                cout << i << ") " << movie << endl;
+                i++;
             }
         }
     }
 
 };
 
+
+class Movie : public MovieSchedule {
+public:
+    string title;
+    string director;
+    string genre;
+    float duration;
+    void Print() {
+        cout << "Title:" << title << "\nDirector:" << director << "\nGenre:" << genre << "\nDuration:" << duration << " hours" << endl;
+    }
+};
+
+
 int main() {
     SetConsoleOutputCP(1251);
     SetConsoleCP(1251);
 
     MovieSchedule schedule;
-    vector<string> movies = { "m1", "m2", "m3" }; 
+    vector<string> movies = { "Людина Павук", "Я, Побєда, і Берлін", "Дюна" };
 
     schedule.display(movies); 
 
+
+    Movie SpiderMan, Berlin, Duna;
+    int number;
+
+    cout << "Оберіть фільм" << endl;
+    cin >> number;
+    
+    switch (number)
+    {
+    case 1:
+        SpiderMan.title = movies[number - 1];
+        SpiderMan.director = "John Weak";
+        SpiderMan.genre = "Mystic";
+        SpiderMan.duration = 1.50;
+        SpiderMan.Print();
+        break;
+    case 2:
+        Berlin.title = movies[number - 1];
+        Berlin.director = "Emil";
+        Berlin.genre = "Mystic";
+        Berlin.duration = 1.50;
+        Berlin.Print();
+        break;
+    case 3:
+        Duna.title = movies[number - 1];
+        Duna.director = "Danil";
+        Duna.genre = "Mystic";
+        Duna.duration = 1.50;
+        Duna.Print();
+        break;
+    default:
+        break;
+    }
+    
+
     return 0;
 }
+
